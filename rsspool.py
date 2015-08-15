44398041cb9d622444d1c410d0ead5549b307323
Chuck <chkengr@gmail.com>
2015-08-15 14:03:39 -0400

    added command line options using optparse

--- rsspool.py
+++ rsspool.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3.4
 
 from multiprocessing import Pool
+import optparse
 import feedparser
 import re
 import time
-import sys
 
 def remove_encoding_errors(text):
     # removing all html tags
@@ -13,19 +13,18 @@ def remove_encoding_errors(text):
     # removing encoding errors
     fixed_text = fixed_text.replace('&rsquo;', "'") 
     fixed_text = fixed_text.replace('&#39;', "'") 
+    fixed_text = fixed_text.replace('&#039;', "'") 
     fixed_text = fixed_text.replace('&nbsp;', " ") 
     fixed_text = fixed_text.replace('&amp;', " and ")
     return fixed_text
 
 def main():
-    if "-p" in sys.argv:
-        argument_index = sys.argv.index("-p") 
-        if len(sys.argv) > argument_index + 1:
-            processes = int(sys.argv[argument_index + 1])
-    else:
-        processes = 4 
+    parser = optparse.OptionParser()
+    parser.add_option("-p", action="store", default=8, type="int", 
+                      help="Number of threads running")
+    options, args = parser.parse_args()
             
-    p = Pool(processes)
+    p = Pool(options.p)
     
     # remember slashdot and ars technica
     with open("url.txt") as u:
@@ -36,7 +35,7 @@ def main():
 
     # retrieving the web pages
     feeds = p.map(feedparser.parse, urls)
-
+    print("Processing feeds")
     entries = [a for b in feeds for a in b.entries]
     title_and_summary = [t.title + '\n' + t.summary for t in entries]
     title_and_summary = [remove_encoding_errors(a) for a in title_and_summary]
@@ -46,13 +45,11 @@ def main():
     titles = [remove_encoding_errors(t.title) for t in entries]
     with open("headline.txt", "w") as h:
         h.write("\n".join(titles))
-    print("processes: ", processes)
+    print("processes: ", options.p)
      
 if __name__ == "__main__":
     start = time.time()
     main()    
-    print("Job took ", str(round(time.time() - start, 2)), "seconds")
-    
-
-
+    total_time = round(time.time() - start, 2)
+    print("Job took ", total_time, "seconds")
 
